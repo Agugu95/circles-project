f9483ca40f6b2790a5e7288b489318cc7da3c271
cooo002 <cooo002@naver.com>
2020-07-13 20:29:02 +0900

    Initial Commit

---
+++ handUP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  handUP
+//
+//  Created by 김재석 on 2020/07/13.
+//  Copyright © 2020 김재석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
